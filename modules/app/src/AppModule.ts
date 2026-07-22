e8a5b6f9f6f5cde668d9d881fe57719f5b5c0ec6
Franck <fkdiomande@icloud.com>
2026-07-22 17:40:52 +0200

    style(app): Adjust array literal formatting in AppModule

--- modules/app/src/AppModule.ts
+++ modules/app/src/AppModule.ts
@@ -1,9 +1,13 @@
 import { SharedModule } from "@module/shared/SharedModule";
 import type { ModuleType } from "@talosjs/module";
 export const AppModule: ModuleType = {
-  controllers: [...SharedModule.controllers],
+  controllers: [...SharedModule.controllers,
+  ],
   entities: [],
-  middlewares: [...SharedModule.middlewares],
-  cronJobs: [...SharedModule.cronJobs],
-  events: [...SharedModule.events],
+  middlewares: [...SharedModule.middlewares,
+  ],
+  cronJobs: [...SharedModule.cronJobs,
+  ],
+  events: [...SharedModule.events,
+  ],
 };
