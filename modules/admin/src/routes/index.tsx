235f97e79e7be623e0e6694582afa7dbdf2e12b5
Franck <fkdiomande@icloud.com>
2026-07-30 17:02:45 +0200

    feat(admin): Enable error, not-found and loader route components

--- modules/admin/src/routes/index.tsx
+++ modules/admin/src/routes/index.tsx
@@ -1,6 +1,14 @@
+import { ErrorFallback } from "@module/design/components/error";
+import { PageLoader } from "@module/design/components/loader";
+import { NotFound } from "@module/design/components/not-found";
 import { createFileRoute } from "@tanstack/react-router";
 
+const RouteNotFound = () => <NotFound />;
+
 export const Route = createFileRoute("/")({
+  notFoundComponent: RouteNotFound,
+  errorComponent: ErrorFallback,
+  pendingComponent: PageLoader,
   component: RouteComponent,
 });
 
