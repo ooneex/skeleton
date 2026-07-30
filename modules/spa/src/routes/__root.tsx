d486f8077f149527745754fe6f77f388da0b116c
Franck <fkdiomande@icloud.com>
2026-07-30 17:02:49 +0200

    feat(spa): Enable error, not-found and loader route components

--- modules/spa/src/routes/__root.tsx
+++ modules/spa/src/routes/__root.tsx
@@ -1,12 +1,17 @@
+import { ErrorFallback } from "@module/design/components/error";
+import { PageLoader } from "@module/design/components/loader";
+import { NotFound } from "@module/design/components/not-found";
 import { TanStackDevtools } from "@tanstack/react-devtools";
 import { hotkeysDevtoolsPlugin } from "@tanstack/react-hotkeys-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools";
 
+const RouteNotFound = () => <NotFound />;
+
 export const Route = createRootRoute({
-  // notFoundComponent: NotFound,
-  // errorComponent: ErrorFallback,
-  // pendingComponent: PageLoader,
+  notFoundComponent: RouteNotFound,
+  errorComponent: ErrorFallback,
+  pendingComponent: PageLoader,
   component: RootComponent,
 });
 
